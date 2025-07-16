e950c86d5a3a92000f7f2de7207111a4cd9743e7
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-15 22:50:57 -0700

    feat: Solve problem 009 - Palindrome Number

---
+++ 1-Easy/009-PalindromeNumber/Solution.cs
@@ -0,0 +1,34 @@
+﻿
+namespace Leetcode._1_Easy._009_PalindromeNumber
+{
+    internal class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            char[] arr = x.ToString().ToArray();
+            char[] newArr = new char[arr.Length];
+
+            string param = x.ToString();
+
+            for (int i = arr.Length; i > 0; i--)
+            {
+                newArr[i - 1] = arr[arr.Length - i];
+
+            }
+
+            string check = new string(newArr);
+
+            if (check == param)
+            {
+                Console.WriteLine($"True. Parameter: {param} is a palindrome of {check}");
+                return true;
+            }
+
+            else
+            {
+                Console.WriteLine($"False. Parameter: {param} is not a palindrome of {check}");
+                return false;
+            }
+        }
+    }
+}
