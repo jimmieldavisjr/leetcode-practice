313966bec4441c0e894719638a2948851aec72cc
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-07-15 22:47:43 -0700

    Restore .gitignore and clean workspace

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Leetcode._1_Easy._009_PalindromeNumber;
+
+Solution solution = new Solution();
+
+solution.IsPalindrome(-121);
