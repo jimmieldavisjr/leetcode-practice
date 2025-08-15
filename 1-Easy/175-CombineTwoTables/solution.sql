952ecebe030f84b76edabb19873c4fce0fc9355d
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-08-14 18:39:59 -0700

    feat: Solve Leetcode 175 - Combine Two Tables - Sql

---
+++ 1-Easy/175-CombineTwoTables/solution.sql
@@ -0,0 +1,41 @@
+﻿-- LeetCode Problem 175: Combine Two Tables
+-- Difficulty: Easy
+-- Date: 2025-08-14
+-- Source: https://leetcode.com/problems/combine-two-tables/
+-- 
+-- Description:
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+-- Return the result table in any order.
+
+--Example:
+--
+/*
+Input: 
+Person table:
++----------+----------+-----------+
+| personId | lastName | firstName |
++----------+----------+-----------+
+| 1        | Wang     | Allen     |
+| 2        | Alice    | Bob       |
++----------+----------+-----------+
+Address table:
++-----------+----------+---------------+------------+
+| addressId | personId | city          | state      |
++-----------+----------+---------------+------------+
+| 1         | 2        | New York City | New York   |
+| 2         | 3        | Leetcode      | California |
++-----------+----------+---------------+------------+
+Output: 
++-----------+----------+---------------+----------+
+| firstName | lastName | city          | state    |
++-----------+----------+---------------+----------+
+| Allen     | Wang     | Null          | Null     |
+| Bob       | Alice    | New York City | New York |
++-----------+----------+---------------+----------+
+*/
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person AS p
+LEFT JOIN Address AS a
+ON p.personId = a.personId;
