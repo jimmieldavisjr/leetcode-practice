c5ee81e976f77eef46c30d7e2c8c492a0b8ffde8
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-03 20:00:14 -0800

    feat(solve): Leetcode Easy 217 Contains Duplicate

---
+++ 1-Easy/217-ContainsDuplicate/Solution_1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode._1_Easy._217_ContainsDuplicate
+{
+    internal class Solution_1
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dict.ContainsKey(nums[i]))
+                {
+                    return true;
+                }
+
+                else
+                {
+                    dict[nums[i]] = i;
+                }
+            }
+
+            return false;
+        }
+    }
+}
