1dfbf78ee58fc756186583220a3f363e1cba4202
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-04 17:38:45 -0800

    feat(solve): Leetcode 290 Word Pattern C#

---
+++ 1-Easy/290-WordPattern/Solution_1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode._1_Easy._290_WordPattern
+{
+    public class Solution
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> dict = new Dictionary<char, string>();
+            string[] words = s.Split(' ');
+
+            if (words.Length != pattern.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (dict.ContainsKey(pattern[i]))
+                {
+                    if (dict[pattern[i]] != words[i])
+                    {
+                        return false;
+                    }
+
+                    else
+                    {
+                        dict[pattern[i]] = words[i];
+                    }
+                }
+
+                else if (!dict.ContainsKey(pattern[i]) && dict.ContainsValue(words[i]))
+                {
+                    return false;
+                }
+
+                else
+                {
+                    dict[pattern[i]] = words[i];
+                }
+            }
+
+            return true;
+        }
+    }
+}
