74b36ad7f625b57a198fc945dbbb7527c3b87137
Jimmie Davis <jimmieldavisjr@outlook.com>
2025-11-06 09:38:02 -0800

    feat(solve): Leetcode 771 Jewels and Stones C#

---
+++ 1-Easy/771-JewelsAndStones/Solution_1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leetcode._1_Easy._771_JewelsAndStones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+
+            int result = 0;
+
+            foreach (char c in jewels)
+            {
+                dict[c] = 0;
+            }
+
+            for (int i = 0; i < stones.Length; i++)
+            {
+                if (dict.ContainsKey(stones[i]))
+                {
+                    dict[stones[i]] += 1;
+                }
+            }
+
+            foreach (var pair in dict)
+            {
+                result += pair.Value;
+            }
+
+            return result;
+        }
+    }
+}
